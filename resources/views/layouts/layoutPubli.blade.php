ab58884d999a5b4afa6a925a6eaa06f6002310ce
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-06-27 15:08:52 -0500

    Footer terminado
    Subí algunas imagenes, cambie css de templatetemo, subí un css, y cambié algunas cosas de las views

--- resources/views/layouts/layoutPubli.blade.php
+++ resources/views/layouts/layoutPubli.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="TemplateMo">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-    <link rel="icon" href="{{ asset('/img/logo.png') }}" type="image/icon type">
+    <link rel="icon" href="{{ asset('/img/logos/logo.png') }}" type="image/icon type">
 
     <script src="https://kit.fontawesome.com/ba2b187421.js" crossorigin="anonymous"></script>
 
@@ -31,6 +31,8 @@ https://templatemo.com/tm-540-lava-landing-page
     <link rel="stylesheet" href="assets/css/templatemo-oscar.css">
 
     <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/footerR.css">
 
     <!-- Fuentes -->
     <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
@@ -327,72 +329,7 @@ https://templatemo.com/tm-540-lava-landing-page
 
 
     <!-- ***** Footer Start ***** -->
-    <footer id="contact-us">
-        <div class="container">
-            <div class="footer-content">
-                <div class="row">
-                    <!-- ***** Contact Form Start ***** -->
-                    <div class="col-lg-6 col-md-12 col-sm-12">
-                        <div class="contact-form">
-                            <form id="contact" action="" method="post">
-                                <div class="row">
-                                    <div class="col-md-6 col-sm-12">
-                                        <fieldset>
-                                            <input name="name" type="text" id="name" placeholder="Full Name" required=""
-                                                style="background-color: rgba(250,250,250,0.3);">
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-md-6 col-sm-12">
-                                        <fieldset>
-                                            <input name="email" type="text" id="email" placeholder="E-Mail Address"
-                                                required="" style="background-color: rgba(250,250,250,0.3);">
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <fieldset>
-                                            <textarea name="message" rows="6" id="message" placeholder="Your Message"
-                                                required="" style="background-color: rgba(250,250,250,0.3);"></textarea>
-                                        </fieldset>
-                                    </div>
-                                    <div class="col-lg-12">
-                                        <fieldset>
-                                            <button type="submit" id="form-submit" class="main-button">Send Message
-                                                Now</button>
-                                        </fieldset>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- ***** Contact Form End ***** -->
-                    <div class="right-content col-lg-6 col-md-12 col-sm-12">
-                        <h2>More About <em>Lava</em></h2>
-                        <p>Phasellus dapibus urna vel lacus accumsan, iaculis eleifend leo auctor. Duis at finibus odio.
-                            Vivamus ut pharetra arcu, in porta metus. Suspendisse blandit pulvinar ligula ut elementum.
-                            <br><br>If you need this contact form to send email to your inbox, you may follow our <a
-                                rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact</a> page
-                            for more detail.</p>
-                        <ul class="social">
-                            <li><a href="https://fb.com/templatemo"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="sub-footer">
-                        <p>Copyright &copy; 2020 Lava Landing Page
-
-                        | Designed by <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('subview.footer')
 
     <!-- jQuery -->
     <script src="assets/js/jquery-2.1.0.min.js"></script>
---
+++ resources/views/subview/footer.blade.php
@@ -0,0 +1,74 @@
+<!-- Footer -->
+<footer class="footer-distributed" style="background-color: #FAFAFA">
+ 
+    <div class="footer-left">
+        <img class="logo145" src="{{ asset('img/logos/logo.png') }}">
+      <p class="footer-company-name" style="margin-top: 5px">© 2020 Editorial uno4cinco</p>
+
+      <a href="http://" target="blank">
+        <p class="footer-company-name dragonware-name">Desarrollado por DragonWare <img class="logodragon" src="{{ asset('img/logos/dragonware.png') }}"></p>
+      </a>
+    </div>
+
+    <div class="footer-center">
+      <div>
+        <p class="footer-titulos">
+            NAVEGACIÓN
+        </p>    
+      </div>
+      <div>
+        <p class="footernav">
+            <a href="">¿QUIÉNES SOMOS?</a><br>
+            <a href="">TIENDA</a><br>
+            <a href="">AUTORES</a><br>
+            <a href="">BLOG</a><br>
+            <a href="">CONTACTO</a><br>
+        </p>
+      </div>
+    </div>
+
+    <div class="footer-centro">
+        <div>
+            <p class="footer-titulos" style="margin-left: 25px">
+                CONTÁCTANOS
+            </p>    
+          </div>
+          <div>
+            {{-- <p class="footernav">
+                <a href=""><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;+524432209371</a><br>
+                <a href=""><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Morelia, Col. Centro, #13, CP. 58170</a><br>
+                <a href=""><img class="logodragon" src="{{ asset('img/ico/email.png') }}">&nbsp; correo@gmail.com</a><br>
+            </p> --}}
+            <ul>
+                <li>
+                    <a href=""><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;+524432209371</a><br>
+                </li>
+                <li>
+                    <a href=""><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Morelia, Col. Centro, #13, CP. 58170</a><br>
+                </li>
+                <li>
+                    <a href=""><img class="logodragon" src="{{ asset('img/ico/email.png') }}">&nbsp; correo@gmail.com</a> <br>
+                </li>
+            </ul>
+          </div>
+    </div>
+
+    <div class="footer-right">
+      <div>
+        <p class="footer-titulos">
+            SOBRE NOSOTROS
+        </p>
+      </div>
+      <div>
+        <p class="footernav">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quibusdam dicta nobis itaque praesentium.
+        </p>
+      </div>  
+
+        <div class="footer-icons">
+            <a href="" target="blank"><img src="{{ asset('img/ico/twt.png')}}" width="22px"></a>
+            <a href="" target="blank"><img src="{{ asset('img/ico/fb.png') }}" width="22px"></a>
+            <a href="" target="blank"><img src="{{ asset('img/ico/ig.png') }}" width="22px"></a>
+        </div>
+    </div>
+  </footer>
