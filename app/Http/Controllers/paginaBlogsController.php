abe3672267047c4a57ccb5c6a3e3333273b3fbe5
PapaDrago <oscarwaii@hotmail.com>
2020-07-07 21:42:40 -0500

    Pues ahí va a la mitad más o menos pero sigámosle echando ganas gente bonita
    view de blogs a la mitad, falta el banner aún, pero ya jala blogs y los ordena chido, falta trabajar responsividad

--- app/Http/Controllers/paginaBlogsController.php
+++ app/Http/Controllers/paginaBlogsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Blog;
 
 class paginaBlogsController extends Controller
 {
     public function index(){
         
-        
+        $blogs=Blog::get();
         //dd($BannerLibros,$BannerAutores,$Libros,$Autores);
-        return view('publicitaria.blogs',[]);
+        return view('publicitaria.blogs',['blogs'=>$blogs]);
     }
 }
--- resources/views/publicitaria/blogs.blade.php
+++ resources/views/publicitaria/blogs.blade.php
@@ -3,6 +3,7 @@
 @section('header')
 <title>Blogs | Editorial uno4cinco</title>
 <link rel="stylesheet" type="text/css" href="/assets/css/style_SobreNosotros.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/blogs.css">
 <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="assetsTimer/fonts/fontawesome/font-awesome.min.css">
@@ -19,32 +20,49 @@
 @endsection
 
 @section('content')
-<!-- ***** Welcome Area Start ***** -->
+    <section class="section" id="about" style="width:100%; height:100%; background-color:#F2ECD5">
+        {{-- TITTLE --}}
+        <p class="txt-TitulosApartados">Blogs</p>
+        <hr class="hr-Titulos-long">
+        <hr class="hr-Titulos-small ">
+        <br><br>
+        {{-- Aquí va el carrusel, esprar a que agustín lo termine de diseñar --}}
 
-<div class="page-wrap" id="root">
-    <!-- Content-->
-    <div class="md-content">
-        <div class="" style="background-color: #F2ECD5">
-            <div class="container">
-                <div class="hero__wrapper">
-                    <p class="txt-TitulosApartados">Blogs</p>
-                    <hr class="hr-Titulos-long">
-                    <hr class="hr-Titulos-small">
+        <div class="blog_encabezado">
+            Todas las entradas
+            <form class="" action="" method="GET" enctype="multipart/form-data">
+                <div class="blog_barra_busqueda">
+                    <select class="busqueda_clasificacion" name="clasificacion" id="tipos_blogs">
+                        <option value="contenido">Contenido</option>
+                        <option value="autor">Autor</option>
+                        <option value="tag">Tag</option>
+                    </select>
+                    <input type="text" id="busqueda_busqueda" class ="" name="busqueda">
+                    <button type="submit" class="busqueda_boton"><i class="fas fa-search"></i></button>
                 </div>
+            </form>
+        </div>
+        <div class="blogs_barra">
+            <div class="blogs_vista">
+                @foreach ($blogs as $blog)
+                    <div class="blog_item">
+                        <div class="blog_item_imagen" style="background: url('{{asset('storage/banners/'.$blog->imagen)}}') center center no-repeat;
+                                background-size: cover;
+                                -moz-background-size: cover;
+                                -o-background-size: cover;
+                                -webkit-background-size: cover;">
+                        </div>
+                        <div class="blog_info">
+                            <div class="blog_info_titulo"> {{Str::limit($blog->titulo,44)}}</div>
+                            {{Str::limit('autor autor autor autor',44)}}<br>
+                            {{Str::limit($blog->fecha,44)}}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="barra_lateral_blogs">
+                
             </div>
         </div>
-    </div>
-    <!-- End / Content-->
-</div>
-<!-- Vendors-->
-<script type="text/javascript" src="assetsTimer/vendors/jquery/jquery.min.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/jquery.countdown/jquery.countdown.min.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/flat-surface-shader/fss.min.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/particles.js/particles.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/waterpipe/waterpipe.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/quietflow/quietflow.min.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/YTPlayer/jquery.mb.YTPlayer.min.js"></script>
-<script type="text/javascript" src="assetsTimer/vendors/vegas/vegas.min.js"></script>
-<!-- App-->
-<script type="text/javascript" src="assetsTimer/js/main.js"></script>
+    </section>
 @endsection
