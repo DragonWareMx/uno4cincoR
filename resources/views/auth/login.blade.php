ac140aa7a0e28f39b12554e3eea4a646b180deb9
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-07-10 14:49:44 -0500

    Login terminado
    Ya terminé la pagina del login, de nada geis, full responsive, ya solo falta la pagina de olvidé mi contraseña y la de restaurar contraseña :'v

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row no-gutter">
-      <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image">
+      <div class="izq-login d-none d-md-flex col-md-4 col-lg-6 bg-image">
         <div class="div-grandote" >
           <img src="{{ asset('img/logos/coloresuno4cinco.png') }}" class="logo-login">
           <div class="linea-login"></div>
@@ -19,31 +19,40 @@
       </div>
       <div class="col-md-8 col-lg-6 cont-der" >
         <div class="login d-flex align-items-center py-5">
+          <div class="container-logo" style="display:none">
+              <img src="{{ asset('img/logos/blancouno4cinco.png') }}">
+          </div>
           <div class="container">
             <div class="row">
               <div class="col-md-9 col-lg-10 mx-auto">
                 <h3 class="login-heading mb-4">Iniciar sesión</h3>
                 <form method="POST" action="{{ route('login') }}">
                 @csrf
-                  <div class="form-label-group">
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" value="{{ old('email') }}" required autofocus>
-                    <label for="inputEmail">Correo Electrónico</label>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                  <div class="centered">
+                    <div class="group">
+                      <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" />
+                      <label for="email">Correo Electrónico</label>
+                      <div class="bar"></div>
+                    </div>
                   </div>
-  
-                  <div class="form-label-group">
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required autocomplete="current-password">
-                    <label for="inputPassword">Contraseña</label>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+
+                <div class="centered">
+                  <div class="group">
+                    <input type="password" id="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <label for="password">Contraseña</label>
+                    <div class="bar"></div>
+                  </div>
                 </div>
+                @error('email')
+                  <div class="error-login" role="alert">
+                    {{ $message }}
+                  </div>
+                    @enderror
+                @error('password')
+                <div class="error-login" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
   
                   <div style="width: 100%;display:flex;justify-content:center;">
                     <button class="button-login" type="submit">Ingresar</button>
