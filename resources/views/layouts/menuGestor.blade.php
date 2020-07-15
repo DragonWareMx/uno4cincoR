e1443622d2882ee0f8cfd7efbbe625b0d47487d6
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-07-14 19:10:17 -0500

    Pequeño arreglo al template del gestor
    Cmabié la foto del usuario :v de nada

--- resources/views/layouts/menuGestor.blade.php
+++ resources/views/layouts/menuGestor.blade.php
@@ -222,7 +222,7 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                <img class="img-profile rounded-circle" style="object-fit:cover;" src="{{asset('/storage/images/usuarios_img/')}}">
+                <img class="img-profile rounded-circle" style="object-fit:cover;" src="{{asset('/img/ico/icons-user.png')}}">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
