8ebc104a95a278cfefdc552a23b529adfc0fd084
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-07-22 22:38:14 -0500

    Avance de las graficas de resumen
    Hola, ya mañana las acabo :v

--- app/Http/Controllers/gestorResumenController.php
+++ app/Http/Controllers/gestorResumenController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class gestorResumenController extends Controller
 {
@@ -12,7 +14,21 @@ class gestorResumenController extends Controller
     }
 
     public function index(){
-
-        return view('gestor.resumen');
+        $today=Carbon::now();
+        $ventas=DB::table('sells')
+                    ->join('book_sell',function($join){
+                        $join->on('sells.id','=','book_sell.sell_id');
+                    })
+                    ->leftJoin('books','books.id','=','book_sell.book_id')
+                    ->get();
+        //dd($ventas);
+        $librosVenta=DB::select('SELECT titulo,COUNT(*) as cantidadN
+        from (sells cross join book_sell on sells.id=book_sell.sell_id ) left join books on books.id=book_sell.book_id 
+        group by titulo order by cantidadN desc');
+        $ciudadesVenta=DB::select('SELECT ciudad,COUNT(*) as cantidadN
+        from (sells cross join book_sell on sells.id=book_sell.sell_id ) left join books on books.id=book_sell.book_id 
+        group by ciudad order by cantidadN desc');
+                    
+        return view('gestor.resumen',['ventas'=>$ventas,'today'=>$today]);
     }
 }
--- resources/views/gestor/resumen.blade.php
+++ resources/views/gestor/resumen.blade.php
@@ -7,7 +7,33 @@
 @endsection
 
 @section('contenido')
+    @php
+        $añoSelec=$today->year;
+        if (isset($_GET['añoSelec']) ) {
+            $añoSelec=$_GET['añoSelec'];
+        }
+    @endphp
     <div class="all_blogs_contenido">
+        <div class="formContainer" style="width:100%">
+            <form class="form-anio" action="" style="width:100%">
+                <p class="tituloResumen2">Año</p>
+                <select class="select-resumen" name="añoSelec" id="">
+                    @php
+                        $valor=(int)$añoSelec-3;
+                        for ($i=0; $i < 7; $i++) { 
+                            if($i!=3){
+                                echo "<option value='".$valor."'>".$valor."</option>";
+                            }
+                            else {
+                                echo "<option value='".$valor."' selected>".$valor."</option>";
+                            }
+                            $valor=$valor+1;
+                        }
+                    @endphp
+                </select>
+                <input class="gestor_blog_guardar" type="submit" value="Buscar">
+            </form>
+        </div>
         {{-- Cuadros para las graficas --}}
         <div class="cuadro-resumen">
             <p class="titulo-resumen">Resumen de ventas</p>
@@ -17,7 +43,7 @@
         </div>
 
         <div class="cuadro-resumen">
-            <p class="titulo-resumen">Total de libros</p>
+            <p class="titulo-resumen">Ventas por género</p>
             <div class="grafica-resumen2">
                 <div class="graficadona-resumen">
                     <canvas id="doughnut-chart"></canvas>
@@ -26,7 +52,7 @@
         </div>
 
         <div class="cuadro-resumen">
-            <p class="titulo-resumen">Total de autores</p>
+            <p class="titulo-resumen">Ventas por edades</p>
             <div class="grafica-resumen2">
                 <div class="graficadona-resumen">
                     <canvas id="doughnut-chart2"></canvas>
@@ -35,7 +61,7 @@
         </div>
 
         <div class="cuadro-resumen">
-            <p class="titulo-resumen">Total de entradas(Blog)</p>
+            <p class="titulo-resumen">Ventas por Ciudad</p>
             <div class="grafica-resumen">
                 <div class="graficadona-resumen">
                     <canvas id="doughnut-chart3"></canvas>
@@ -44,7 +70,7 @@
         </div>
 
         <div class="cuadro-resumen">
-            <p class="titulo-resumen">Últimos</p>
+            <p class="titulo-resumen">Libros más vendidos</p>
             <div class="ultimos-resumen">
                 <table class="tabla-resumen">
                     <tr>
@@ -76,23 +102,70 @@
             </div>
         </div>
     </div>
+@php
+    $sumaVentasTotales=[
+      '01'=>0,
+      '02'=>0,
+      '03'=>0,
+      '04'=>0,
+      '05'=>0,
+      '06'=>0,
+      '07'=>0,
+      '08'=>0,
+      '09'=>0,
+      '10'=>0,
+      '11'=>0,
+      '12'=>0
+    ];
+    $sumaGeneros=[
+      'hombre'=>0,
+      'mujer'=>0,
+      'otros'=>0,
+    ];
+@endphp
+@foreach ($ventas as $venta)
+    @php
+        $monthf = date('m', strtotime($venta->fecha));
+        $yearf = date('y', strtotime($venta->fecha)) + 2000;
+    @endphp
+    @if ($yearf==$añoSelec)
+        @php
+            $sumaVentasTotales[$monthf]+=$venta->precio;
+        @endphp
+  @endif
+@endforeach
+
+@foreach ($ventas as $venta)
+    @if ($venta->genero=='Hombre')
+        @php
+            $sumaGeneros['hombre']+=1;  
+        @endphp
+    @elseif($venta->genero=='Mujer')
+        @php
+            $sumaGeneros['mujer']+=1;  
+        @endphp
+    @elseif($venta->genero=='Otros')
+        @php
+            $sumaGeneros['otros']+=1;  
+        @endphp
+    @endif    
+@endforeach
 
     <script>
+        var sumaVentasTotales=<?php echo json_encode($sumaVentasTotales); ?>;
+        var sumaGeneros=<?php echo json_encode($sumaGeneros); ?>;
+        var ano=<?php echo $añoSelec ?>;
         new Chart(document.getElementById("line-chart"), {
         type: 'line',
         data: {
             labels: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
             datasets: [{ 
-                data: [12,11,10,9,8,7,6,5,4,3,2,1
+                data: [sumaVentasTotales['01'],sumaVentasTotales['02'],sumaVentasTotales['03'],sumaVentasTotales['04'],
+                sumaVentasTotales['05'],sumaVentasTotales['06'],sumaVentasTotales['07'],sumaVentasTotales['08'],
+                sumaVentasTotales['09'],sumaVentasTotales['10'],sumaVentasTotales['11'],sumaVentasTotales['12']
                 ],
                 label: "Ingresos",
                 borderColor: "#1cc88a",
-                fill: false
-            }, { 
-                data: [1,2,3,4,5,6,7,8,9,10,11,12
-                ],
-                label: "Egresos",
-                borderColor: "#e74a3b",
                 fill: false
             }
             ]
@@ -100,7 +173,7 @@
         options: {
             title: {
             display: true,
-            text: 'Ingresos de ' + 2020
+            text: 'Ingresos por ventas de ' + ano
             },
             maintainAspectRatio:false,
             responsive:true
@@ -109,12 +182,12 @@
         new Chart(document.getElementById("doughnut-chart"), {
         type: 'doughnut',
         data: {
-        labels: ["Ingresos", "Egresos"],
+        labels: ["Hombres", "Mujeres","Otros"],
         datasets: [
             {
             label: "$",
-            backgroundColor: ["#1cc88a", "#e74a3b"],
-            data: [25,35]
+            backgroundColor: ["#33C6FF", "#FE8ED4","#5F72A4"],
+            data: [sumaGeneros['hombre'],sumaGeneros['mujer'],sumaGeneros['otros']]
             }
         ]
         },
