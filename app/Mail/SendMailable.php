69eee04d36f5bcf28fedda801cfa1c434375a565
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-08-10 15:04:36 -0500

    aiuda da error
    hola

--- app/Mail/SendMailable.php
+++ app/Mail/SendMailable.php
@@ -35,7 +35,19 @@ class SendMailable extends Mailable
         $sell=Sell::findOrFail($this->idVenta);
         $librosVendidos=Book_Sell::where('sell_id',$sell->id)->get();
         $libros=Book::get();
+        $links=[];
+        $cont=0;
+        foreach ($librosVendidos as $libro) {
+            if($libro->digital==1){
+                $link=\Linkeys\UrlSigner\Facade\UrlSigner::generate(route('descargar'), ['id_libro' => $libro->book_id], '+5040 hours', 3);
+                $links[$cont]=$link->getFullUrl();
+            }
+            else{
+                $links[$cont]=null;
+            }
+            $cont++;
+        }
         return $this->subject('Su compra en uno4cinco se ha realizado con éxito!')
-        ->view('emails.pedidoemail',['sell'=>$sell,'librosVendidos'=>$librosVendidos,'libros'=>$libros]);
+        ->view('emails.pedidoemail',['sell'=>$sell,'librosVendidos'=>$librosVendidos,'libros'=>$libros,'links'=>$links]);
     }
 }
