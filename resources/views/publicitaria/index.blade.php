9d9601bf2982b43183030e81f5802a202e0b2877
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-08-13 12:34:59 -0500

    Ya dejé creada la vista para lo del aviso de privacidad
    Cambiale kkstin, de nada

--- resources/views/publicitaria/index.blade.php
+++ resources/views/publicitaria/index.blade.php
@@ -786,7 +786,7 @@
     <div id="cajacookies">
         <p><button onclick="aceptarCookies()" class="pull-right"><i class="fa fa-times"></i> Aceptar y cerrar este mensaje</button>
         Este sitio web usa cookies, si permanece aquí acepta su uso.
-        Puede leer más sobre el uso de cookies en nuestra <a href="politica.html">política de privacidad</a>.
+        Puede leer más sobre el uso de cookies en nuestra <a href="{{route('avisoPrivacidad')}}">política de privacidad</a>.
         </p>
     </div>
 
---
+++ resources/views/publicitaria/privacidad.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.layoutPubli')
+
+@section('header')
+<title>Política de privacidad | Editorial uno4cinco</title>
+
+<link rel="stylesheet" type="text/css" href="/assets/css/style_SobreNosotros.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+@endsection
+
+@section('content')
+
+<section class="section" id="about" style="width:100%; height:100%; background-color:#F2ECD5">
+    {{-- TITTLE --}}
+    <p class="txt-TitulosApartados">Política de privacidad y uso de cookies</p>
+    <hr class="hr-Titulos-long">
+    <hr class="hr-Titulos-small">
+    {{-- BANNER IMAGE --}}
+
+    
+
+    <p class="p-txt-content">
+        Tu descarga debería empezar automaticamente. <br> En caso de que no se haya descargado, por favor use este enlace. 
+    </p>
+
+    
+</section>
+
+@endsection
