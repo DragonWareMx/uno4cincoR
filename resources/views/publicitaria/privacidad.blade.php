c263af6a9c40b6e4e94b5186191a4cfe0b7f56c9
agussolorzano <joseagustinsolorzano@gmail.com>
2020-08-14 13:55:15 -0500

    Aviso de privacidad
    Listo aviso de privacidad hdspm

--- resources/views/publicitaria/privacidad.blade.php
+++ resources/views/publicitaria/privacidad.blade.php
@@ -22,11 +22,96 @@
 
     
 
-    <p class="p-txt-content">
-        Tu descarga debería empezar automaticamente. <br> En caso de que no se haya descargado, por favor use este enlace. 
+    <p class="p-txt-content" style="text-align: justify">
+        De conformidad con lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, uno4cinco pone a su disposición el siguiente aviso de privacidad.
+        Uno4cinco es responsable del uso y protección de sus datos personales, en este sentido y atendiendo las obligaciones legales establecidas en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, a través de este instrumento se informa a los titulares de los datos, la información que de ellos se recaba y los fines que se le darán a dicha información.
+        Además de lo anterior, informamos a usted que uno4cinco tiene su domicilio ubicado en:
     </p>
-
-    
+    <p class="p-txt-content" style="text-align: justify">
+        Ramón Gómez del Villar, Colonia Héroes del 5 de Mayo, Toluca, México.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Los datos personales que recabamos de usted serán utilizados para las siguientes finalidades, las cuales son necesarias para concretar nuestra relación con usted así como atender los servicios y/o pedidos que solicite:
+        Vender productos libros tanto en formato digital como en formato físico. 
+    <p class="p-txt-content" style="text-align: justify">
+        Sus datos también podrán ser utilizados con fines estadísticos para que uno4cinco mejore su servicio y sus productos. 
+        Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:
+        Información que nos proporciona. Cuando llena el formulario previo a la realización de una compra, podemos recopilar y almacenar la Información personal que usted proporciona, que incluye:
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Identificadores personales<br>
+        Recibimos identificadores personales que usted proporciona (incluida, por ejemplo, información como su nombre, ciudad de residencia, dirección de correo electrónico, número de teléfono o identificadores similares) y los usamos para responder y completar sus solicitudes, proporcionar notificaciones y comunicarnos con usted para concretar la venta, el servicio al cliente y para fines informativos (incluido el envío de correos electrónicos de confirmación, avisos, actualizaciones, facturas y alertas de seguridad) y para fines de marketing (incluido el envío de noticias, promociones y otra información que nosotros y nuestros afiliados y los socios creen que pueden ser de su interés). También podemos crear códigos de identificación y referencia internos para identificarlo en nuestros sistemas electrónicos.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Comunicaciones y envíos<br>
+        Recopilamos la información y el contenido que envía cuando se realiza una compra. Utilizamos esta información para responder y cumplir con sus solicitudes, comunicarnos con usted sobre el Servicio, con fines promocionales / de marketing, para establecer y administrar nuestras relaciones con los clientes y el servicio al cliente, y para proporcionar y mejorar nuestros sitios web, productos y servicios.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Información financiera<br>
+        Recibimos información financiera, como tarjeta de pago, información bancaria o información de PayPal ™, y otra información relacionada para procesar los pagos de los productos que ha solicitado. 
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Información comercial<br>
+        Recopilamos y generamos información comercial e información de transacciones (como registros de productos comprados en nuestros sitios web, preferencias de nuestros productos). Utilizamos esta información para responder y cumplir solicitudes, mejorar nuestras ofertas de productos y servicios, comunicarnos con usted, adaptar nuestros esfuerzos de marketing y para fines de informes internos.
+        Utilizaremos la información enumerada anteriormente para hacer inferencias sobre sus preferencias para ayudarnos a desarrollar y proporcionar mejores productos y servicios.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Redes sociales y datos disponibles al público<br>
+        Nuestros sitios web incluyen funciones de redes sociales, como el botón “Me gusta” de Facebook y widgets como el botón “Compartir” y funciones similares. Estas funciones pueden recopilar su dirección IP, las páginas que está visitando en nuestros sitios web y pueden establecer cookies para que funcionen como usted lo solicitó. Las características y los widgets de las redes sociales son alojados por un tercero o directamente en nuestros sitios web. Usamos la información personal recopilada de esta manera para los mismos fines descritos anteriormente.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Otra información que recopilamos<br>
+        También podemos recopilar otra información sobre usted, su dispositivo o su uso de los servicios de la manera que le describimos en el momento de la recopilación o de otra manera con su consentimiento. Puede optar por no proporcionarnos ciertos tipos de información, pero hacerlo puede afectar su capacidad de utilizar algunos de los servicios de nuestra plataforma. 
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Usted tiene en todo momento el derecho a conocer qué datos personales tenemos de usted, 
+        para qué los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, 
+        es su derecho solicitar la corrección de su información personal en caso de que esté 
+        desactualizada, sea inexacta o incompleta (Rectificación); de igual manera, tiene derecho 
+        a que su información se elimine de nuestros registros o bases de datos cuando considere que 
+        la misma no está siendo utilizada adecuadamente (Cancelación); así como también a oponerse 
+        al uso de sus datos personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO.<br>
+        Para el ejercicio de cualquiera de los derechos ARCO, se deberá presentar la solicitud respectiva a través del 
+        siguiente correo electrónico:
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        contacto@uno4cinco.com
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Lo anterior también servirá para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO.<br>
+        
+        En todo caso la respuesta a la solicitud se dará en un lapso no mayor a 15 días. <br>
+        
+        Los datos de contacto de la persona o departamento de datos personales, que está a cargo de dar trámite a las solicitudes de derechos ARCO, son los siguientes:<br>
+         
+        a) Nombre del responsable: César Jordán <br>
+        b) Domicilio: <br>
+        Ramón Gómez del Villar, Colonia Héroes del 5 de Mayo, Toluca, México<br>
+        c) Teléfono: 7221834383 <br>
+        
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Cabe mencionar, que en cualquier momento usted puede revocar su consentimiento para el uso de sus datos personales. Del mismo modo, usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligación legal se requiera seguir tratando sus datos personales. Asimismo, usted deberá considerar que para ciertos fines, la revocación de su consentimiento implicará que no podamos seguir prestando el servicio que nos solicitó, o la conclusión de su relación con nosotros.
+        Para revocar el consentimiento que usted otorga en este acto o para limitar su divulgación, se deberá presentar la solicitud respectiva a través del siguiente correo electrónico:
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        contacto@uno4cinco.com
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Del mismo modo, podrá solicitar la información para conocer el procedimiento y requisitos para la revocación del consentimiento, así como limitar el uso y divulgación de su información personal.
+        En cualquier caso, la respuesta a las peticiones se dará a conocer en un plazo de 15 días.
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas, por lo cual, nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad, sin embargo, usted puede solicitar información sobre si el mismo ha sufrido algún cambio a través del siguiente correo electrónico:
+        contacto@uno4cinco.com
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        Última actualización:
+        20/08/2020
+    </p>
+    <p class="p-txt-content" style="text-align: justify">
+        
+    </p> 
 </section>
 
 @endsection
