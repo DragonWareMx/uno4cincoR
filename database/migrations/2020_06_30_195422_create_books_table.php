86c7b4e058fec261fe79aad42f5db98ea60fbf65
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2022-03-07 22:36:21 -0600

    nullable precio del audiolibro

--- database/migrations/2020_06_30_195422_create_books_table.php
+++ database/migrations/2020_06_30_195422_create_books_table.php
@@ -19,7 +19,7 @@ class CreateBooksTable extends Migration
             $table->text('titulo');
             $table->string('numEdicion',3);
             $table->float('precioFisico');
-            $table->float('precioAudio');
+            $table->float('precioAudio')->nullable();
             $table->float('precioDigital');
             $table->float('descuentoFisico');
             $table->float('descuentoDigital');
