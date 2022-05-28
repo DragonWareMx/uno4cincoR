dd77dc1a3ff180845f052adc1e08cf129bc25d80
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2022-05-27 20:14:00 -0500

    redes sociales

--- resources/views/subview/footer.blade.php
+++ resources/views/subview/footer.blade.php
@@ -43,16 +43,16 @@
           </div>
           <div>
             {{-- <p class="footernav">
-                <a href=""><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;+524432209371</a><br>
-                <a href=""><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Morelia, Col. Centro, #13, CP. 58170</a><br>
+                <a href=""><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;722 6145 553</a><br>
+                <a href=""><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Montecito 38, Nápoles, Benito Juárez, 03810 Ciudad de México, CDMX</a><br>
                 <a href=""><img class="logodragon" src="{{ asset('img/ico/email.png') }}">&nbsp; correo@gmail.com</a><br>
             </p> --}}
             <ul>
                 <li>
-                    <a href="tel:7221834383"><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;7221834383</a><br>
+                    <a href="tel:7221834383"><img class="logodragon" src="{{ asset('img/ico/phone.png') }}">&nbsp;722 6145 553</a><br>
                 </li>
                 <li>
-                    <a href="https://www.google.com/maps/place/Toluca+de+Lerdo,+M%C3%A9x./@19.294109,-99.6662331,13z/data=!3m1!4b1!4m5!3m4!1s0x85cd89892a50ebb9:0xad3f4ad5550208c4!8m2!3d19.2826098!4d-99.6556653" target="_blank"><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Toluca, México</a><br>
+                    <a href="https://www.google.com/maps/place/Toluca+de+Lerdo,+M%C3%A9x./@19.294109,-99.6662331,13z/data=!3m1!4b1!4m5!3m4!1s0x85cd89892a50ebb9:0xad3f4ad5550208c4!8m2!3d19.2826098!4d-99.6556653" target="_blank"><img class="logodragon" src="{{ asset('img/ico/gps.png') }}">&nbsp;Montecito 38, Nápoles, Benito Juárez, 03810 Ciudad de México, CDMX</a><br>
                 </li>
                 <li>
                     <a href="mailto: contacto@uno4cinco.com"><img class="logodragon" src="{{ asset('img/ico/email.png') }}">&nbsp;  contacto@uno4cinco.com</a> <br>
@@ -75,10 +75,10 @@
       </div>  
 
         <div class="footer-icons">
-            <a href="https://www.youtube.com/channel/UCuYHXFV2FXf76TyP3aKSEqQ" target="blank"><img src="{{ asset('img/ico/ytb.png')}}" width="22px"></a>
-            <a href="https://www.facebook.com/uno4cinco" target="blank"><img src="{{ asset('img/ico/fb.png') }}" width="22px"></a>
-            <a href="https://www.instagram.com/uno4cinco" target="blank"><img src="{{ asset('img/ico/ig.png') }}" width="22px"></a>
-            <a href="https://www.tiktok.com/@amorfatimx" target="blank"><img src="{{ asset('img/ico/tiktok.png') }}" width="20x"></a>
+            <!-- <a href="https://www.youtube.com/channel/UCuYHXFV2FXf76TyP3aKSEqQ" target="blank"><img src="{{ asset('img/ico/ytb.png')}}" width="22px"></a> -->
+            <a href="https://www.facebook.com/El-Booke-404763476744688" target="blank"><img src="{{ asset('img/ico/fb.png') }}" width="22px"></a>
+            <a href="https://www.instagram.com/elbooke/" target="blank"><img src="{{ asset('img/ico/ig.png') }}" width="22px"></a>
+            <a href="https://www.tiktok.com/@el_booke" target="blank"><img src="{{ asset('img/ico/tiktok.png') }}" width="20x"></a>
         </div>
     </div>
   </footer>
