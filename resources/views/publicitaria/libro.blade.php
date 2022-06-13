4fd76ae5060712daca12f51bb08c2f982f45a911
Leonardo Daniel López López <lopez_lopez_daniel@hotmail.com>
2022-06-13 02:20:58 -0500

    cambios ultimos

--- resources/views/publicitaria/libro.blade.php
+++ resources/views/publicitaria/libro.blade.php
@@ -357,6 +357,87 @@
         -ms-transition: all 0.3s cubic-bezier(1, .01, .32, 1);
         transition: all 0.3s cubic-bezier(1, .01, .32, 1);
     }
+
+    .share-button{
+        border: 1px solid #1FC6AC;
+        color: #1FC6AC;
+        border-radius: 25px;
+        display: inline-block;
+        position: absolute;
+        overflow: hidden;
+        width: 178px;
+        height: 40px;
+        top: 50%;
+        left: 50%;
+        text-transform: uppercase;
+        font-size: 14px;
+        transform: translate(-50%, -50%);
+        font-family: 'Montserrat'
+    }
+    .share-button a{
+        color: #1FC6AC;
+        font-size: 12px;
+        display: block;
+        text-decoration: none;
+        font-family: 'Montserrat';
+        font-weight: bold;
+        letter-spacing: 1px;
+        line-height: 26px;
+        padding: 7px 13px 7px 13px;
+        text-transform: capitalize;
+    }
+    
+    .share-button > a:hover,
+    .share-button > a:hover:after,
+    .share-button .icon-wrapper ul li a:hover i{
+        color: #1FC6AC;
+    }
+    .share-button .icon-wrapper ul li:last-child a:hover i{
+        color: back;
+    }
+    .share-button .icon-wrapper{
+        position: absolute;
+        left: -286px;
+        top: 0;
+        width: 286px;
+    }
+
+    .share-button .icon-wrapper > a{
+        display: inline-block;
+    }
+    .share-button a.hidden{
+        display: none;
+    }
+    .share-button.active .icon-wrapper{
+        max-height: 40px;
+    }
+    .share-button .icon-wrapper ul{
+        padding-left: 0;
+        margin: 0;
+        height: 40px;
+    }
+    .share-button .icon-wrapper ul:after{
+        clear: both;   
+    }
+    .share-button .icon-wrapper ul li{
+        float: left;
+        display: inline-block;
+    }
+    .share-button .icon-wrapper ul li:last-child{
+        float: right;
+    }
+    .share-button .icon-wrapper ul li a i{
+        color: #1FC6AC;
+        font-size: 14px;
+    }
+    .share-button .icon-wrapper ul li:last-child a{
+        background-color: #1FC6AC;
+    }
+    .share-button .icon-wrapper ul li:last-child a i{
+        color: #2d3847;
+        position: relative;
+        left: 1px;
+    }
 </style>
 @endsection
 
@@ -927,9 +1008,23 @@
 
             {{-- BOTON COMPARTIR CELULAR --}}
             <div class="col ld-inverso" id="col-carrusel">
-                <div style="width: 100%; display: flex; justify-content: center">
+                {{-- <div style="width: 100%; display: flex; justify-content: center">
                     <button class="ld-button share-button">Compartir este libro</button>
+                </div> --}}
+                
+                <div class="share-button">
+                    <a href="#">Compartir este libro</a>
+                    <div class="icon-wrapper">
+                        <ul>
+                            <li><a class="twitter customer share" href="{{"https://twitter.com/share?url=".URL::current()."&amp"}}" title="Twitter share" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a target="_blank" href="{{"https://www.facebook.com/sharer.php?u=".URL::current()}}" title="Facebook share" target="_blank" class="customer share"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="{{"mailto:?subject=".$book->titulo."-Booke&body=Comprar ".$book->titulo. "  ".URL::current()}}"><i class="fa fa-envelope"></i></a></li>
+                            <li><a href="{{URL::current()}}" class="copy_text"><i class="fa fa-link"></i></a></li>
+                            <li><a href="#" class="close-social-icons"><i class="fa fa-times"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
+
             </div>
         </div>
 
@@ -1976,20 +2071,115 @@ function pagination(){
 
 </script>
 
-<script>
+{{-- <script>
     const shareButton = document.querySelector('.share-button');
 
     shareButton.addEventListener('click', event => {
-    if (navigator.share) {
-    navigator.share({
-        title: 'Booke | Libro' ,
-        url: window.url
-        }).then(() => {
-        console.log('Gracias por compartir!');
-        })
-        .catch(console.error);
+    var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        isMobile = true;
+    }
+    if (isMobile && navigator.share) {
+        navigator.share({
+            title: 'Booke | Libro' ,
+            url: window.url
+            }).then(() => {
+            console.log('Gracias por compartir!');
+            })
+            .catch(console.error);
         }
     });
+</script> --}}
+
+<script>
+    postShareButtonClick = $(function (){
+    var buttonWrapper = $(".share-button"),
+        button = $(".share-button > a"),
+        icons = $(".share-button > .icon-wrapper"),
+        close = $(".close-social-icons");
+    
+    function init(){
+        button.on("click", toggle);
+        close.on("click", closeIcons);
+    }
+    
+    function toggle(e){
+        if (buttonWrapper.hasClass("active")){
+            closeIcons();
+        } else{
+            openIcons();
+        }
+        e.preventDefault();
+    }
+    
+    function openIcons(){
+        buttonWrapper.addClass("active");
+        button.addClass("hidden");
+        buttonWrapper.animate({width: "286"}, 500);
+        icons.animate({left: "0"}, 500);
+    }
+    
+    function closeIcons(){
+        buttonWrapper.removeClass("active");
+		button.removeClass("hidden");
+        icons.animate({left: "-286"}, 0);
+        buttonWrapper.animate({width: "178"}, 0);
+    }
+
+    $('.copy_text').click(function (e) {
+        e.preventDefault();
+        var copyText = $(this).attr('href');
+
+        document.addEventListener('copy', function(e) {
+            e.clipboardData.setData('text/plain', copyText);
+            e.preventDefault();
+        }, true);
+
+        document.execCommand('copy');  
+    });
+    
+    init();
+});
+</script>
+
+<script>
+    (function($){
+  
+  /**
+   * jQuery function to prevent default anchor event and take the href * and the title to make a share popup
+   *
+   * @param  {[object]} e           [Mouse event]
+   * @param  {[integer]} intWidth   [Popup width defalut 500]
+   * @param  {[integer]} intHeight  [Popup height defalut 400]
+   * @param  {[boolean]} blnResize  [Is popup resizeabel default true]
+   */
+  $.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
+    
+    // Prevent default anchor event
+    e.preventDefault();
+    
+    // Set values for window
+    intWidth = intWidth || '500';
+    intHeight = intHeight || '400';
+    strResize = (blnResize ? 'yes' : 'no');
+
+    // Set title and open popup with focus on it
+    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
+        strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,            
+        objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
+  }
+  
+  /* ================================================== */
+  
+  $(document).ready(function ($) {
+    $('.customer.share').on("click", function(e) {
+      $(this).customerPopup(e);
+    });
+  });
+    
+}(jQuery));
 </script>
 
 @endsection
